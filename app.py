c3dad4f173d56d0377cf9d9c1ff7daea016a12bf
Valmik J <mail@valmik.in>
2020-03-09 01:22:26 +0530

    feat: add random quote generator

--- app.py
+++ app.py
@@ -5,66 +5,38 @@ import sys
 import pymysql
 import redis
 
+import falcon
+
 from platformshconfig import Config
 
 
 config = Config()
 
-
-def wrap_test(callback, *args, **kwargs):
-    try:
-        result = callback(*args, **kwargs)
-        return {
-            "status": "OK",
-            "return": result,
-        }
-    except Exception:
-        return {
-            "status": "ERROR",
-            "error": traceback.format_exception(*sys.exc_info())
-        }
+class QuoteResource:
+    def on_get(self, req, resp):
+        resp.media = get_quote()
 
 
-def test_mysql(instance):
+
+def get_quote():
+    database_config = config.credentials('database')
     connection = pymysql.connect(
-        host=instance["host"],
-        port=instance["port"],
-        user=instance["username"],
-        password=instance["password"],
-        db=instance["path"],
+        host=database_config["host"],
+        port=database_config["port"],
+        user=database_config["username"],
+        password=database_config["password"],
+        db=database_config["path"],
         charset='utf8mb4',
         cursorclass=pymysql.cursors.DictCursor,
     )
 
     try:
         with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-            cursor.fetchone()
+            cursor.execute("SELECT * FROM quotes ORDER BY RAND() LIMIT 1;")
+            return cursor.fetchone()
 
     finally:
         connection.close()
 
-
-def test_redis(instance):
-    r = redis.StrictRedis(
-        host=instance["host"],
-        port=instance["port"],
-        db=0,
-    )
-    key_name = "foo-%s" + str(uuid.uuid4())
-    value = b"bar"
-
-    r.set(key_name, "bar")
-    assert value == r.get(key_name)
-
-
-# uwsgi requires the WSGI entry point to be named "application"
-def application(env, start_response):
-
-    tests = {
-        "mysql": wrap_test(test_mysql, config.credentials("database")),
-        "redis": wrap_test(test_redis, config.credentials("rediscache"))
-    }
-
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return [bytes(str(tests), "utf8")]
+application = falcon.API()
+application.add_route('/api/quote', QuoteResource())
