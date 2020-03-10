6c0cbe2cf59c6c73c999088a7aa42d001fff0d83
Valmik J <mail@valmik.in>
2020-03-10 17:06:55 +0530

    fix: use v-cloak to show loading

--- public/script.js
+++ public/script.js
@@ -3,15 +3,12 @@ new Vue({
     data() {
         return {
             quote: null,
-            loading: false
         }
     },
     mounted() {
-        this.loading = true
         axios
             .get('/api/quote')
             .then(response => {
-            this.loading = false
             this.quote = response.data
             })
     }
